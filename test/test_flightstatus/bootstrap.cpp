33b65815676cdb895362342cda79319fe54b1770
Alex <korte@clemson.edu>
2023-10-19 20:02:12 -0400

    Added Unity Testing

--- test/test_flightstatus/bootstrap.cpp
+++ test/test_flightstatus/bootstrap.cpp
@@ -1,31 +1,29 @@
-#include <gtest/gtest.h>
+#include "unity.h"
 
 #if defined(ARDUINO)
 #include <Arduino.h>
 
 #include "telemetry.h"
 #include "flightstatus.h"
+#include "test_flightstatus.cpp"
+
+int runUnityTests(void) {
+  UNITY_BEGIN();
+    RUN_TEST(test_initial_stage_is_armed);
+    RUN_TEST(test_stage_transitions);
+    RUN_TEST(test_apogee_detection);
+    RUN_TEST(test_ground_detection);
+    return UNITY_END();
+}
 
 void setup()
 {
-    // should be the same value as for the `test_speed` option in "platformio.ini"
-    // default value is test_speed=115200
-    Serial.begin(115200);
-
-    ::testing::InitGoogleTest();
-    // if you plan to use GMock, replace the line above with
-    // ::testing::InitGoogleMock();
+    delay(2000);
+    
+    runUnityTests();
 }
 
-void loop()
-{
-  // Run tests
-  if (RUN_ALL_TESTS()) {}
-  ;
-
-  // sleep for 1 sec
-  delay(1000);
-}
+void loop() {}
 
 #else
 int main(int argc, char **argv)
