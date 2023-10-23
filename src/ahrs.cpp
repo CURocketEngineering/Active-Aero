2e1f29b6156f41374036f280a94f58edc6e52b73
Sam Pupke <sampupke@gmail.com>
2023-10-23 16:32:25 -0400

    add ahrs header

---
+++ src/ahrs.cpp
@@ -0,0 +1,37 @@
+#include "ahrs.h"
+
+void AHRS::stateUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz) {
+
+}
+
+
+float getAccelY(){
+
+}
+
+float getAccelZ(){
+
+}
+
+float getAccelX(){
+
+}
+
+void getHeading(){
+
+}
+
+void getGravityVector(){
+
+}
+
+void getQuaternion(float* w, float* x, float* y, float* z){
+
+
+}
+
+void setQuaternion(float w, float x, float y, float z) {
+
+
+
+}
---
+++ src/ahrs.h
@@ -0,0 +1,34 @@
+#ifndef AHRS_H
+#define AHRS_H
+
+class AHRS {
+   public:
+   //talk w MD if he wants more or less than these, or any differences
+   //
+    
+    void setQuaternion(float w, float x, float y, float z); //should set @ start of launch
+
+    //include v in stateUpdate when/if I can get v integrated into the 3D EKF
+    //can samples get thrown off if some but not all get changed in a tick? NS sampling thm
+    void stateUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz); 
+
+    //true accel from YPR + Axyz
+
+    float getAccelY();
+
+    float getAccelZ();
+
+    float getAccelX();
+
+    void getHeading(); //some heading pointer depends on how they want the values, if at all
+
+    void getGravityVector(); //not sure how different this is from accelY but the library has support for both so ill provide access to both
+
+    void getQuaternion(float* w, float* x, float* y, float* z);
+
+    //heading - angle of depression or elevation --> yaw --> not yaw
+
+    
+};
+
+#endif
