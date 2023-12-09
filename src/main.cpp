297d0d3604ef87cc17dc9ee3605a58a0d25d78a2
Alex <alex.k.korte@gmail.com>
2023-12-09 05:51:58 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,289 @@
 #include <Arduino.h>
 
-#include "telemetry.h"
-#include "apogeeprediction.h"
-#include "flightstatus.h"
-#include "sdlogger.h"
-#include "ahrs.h"
+/*
+ * Connect the SD card to the following pins:
+ *
+ * SD Card | ESP32
+ *    D2       -
+ *    D3       SS
+ *    CMD      MOSI
+ *    VSS      GND
+ *    VDD      3.3V
+ *    CLK      SCK
+ *    VSS      GND
+ *    D0       MISO
+ *    D1       -
+ */
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
 
-SDLogger sdLogger;
-Telemetry telemetry;
-AHRS ahrs;
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\n", dirname);
 
-double baseAlt;
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
 
-void setup() {
-  sleep(5);
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  while (!Serial) {}
-  Serial.println("Starting up");
-
-  sdLogger.setup();
-
-  telemetry.setupSensors();
-  sdLogger.writeLog("Setup complete");
-  Serial.println(telemetry.getSensorConfig().c_str());
-  sdLogger.writeLog(telemetry.getSensorConfig());
-
-  ahrs.begin(115200);//sample frequency 
-  ahrs.setRotationVector(0,0,0);
-  
-  Serial.println("Finished setup");
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.path(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
 }
 
-void loop() {
-  Serial.println("Getting Telemetry");
-  TelemetryData telemData = telemetry.getTelemetry();
-  Serial.println("Got Telemetry");
-  Serial.println(telemData.sensorData["acceleration"].acceleration.x);
-  Serial.println(telemData.sensorData["acceleration"].acceleration.y);
-  Serial.println(telemData.sensorData["altitude"].altitude);
-
-  Serial.println("Updating AHRS State");
-  
-  
-  ahrs.update(telemData.sensorData["gyro"].gyro.x, 
-              telemData.sensorData["gyro"].gyro.y, 
-              telemData.sensorData["gyro"].gyro.z, 
-              telemData.sensorData["acceleration"].acceleration.x, 
-              telemData.sensorData["acceleration"].acceleration.y, 
-              telemData.sensorData["acceleration"].acceleration.z, 
-              telemData.sensorData["magnetometer"].magnetic.x, 
-              telemData.sensorData["magnetometer"].magnetic.y, 
-              telemData.sensorData["magnetometer"].magnetic.z);
-  Serial.println("AHRS State Updated");
-  
-  float rx, ry, rz;
-  ahrs.getRotationVector(&rx,&ry,&rz);
-  Serial.printf("rx=%f \try=%f \trz=%f", rx, ry, rz);
-  float gx, gy, gz;
-  ahrs.getGravityVector(&gx, &gy, &gz);
-  Serial.printf("\tgx=%f \tgy=%f \tgz=%f", gx, gy, gz);
-  float ax, ay, az;
-  Serial.printf("\tax=%f \tay=%f \taz=%f\n", ax, ay, az);
-  /*Serial.printf("mx=%f \tmy=%f \tmz=%f\n",telemData.sensorData["magnetometer"].magnetic.x, 
-              telemData.sensorData["magnetometer"].magnetic.y, 
-              telemData.sensorData["magnetometer"].magnetic.z);*/ //having some issues where the magnetometer isnt always connected
-  sleep(0.1);
+void createDir(fs::FS &fs, const char * path){
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
 }
+
+void removeDir(fs::FS &fs, const char * path){
+    Serial.printf("Removing Dir: %s\n", path);
+    if(fs.rmdir(path)){
+        Serial.println("Dir removed");
+    } else {
+        Serial.println("rmdir failed");
+    }
+}
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+    file.close();
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+    Serial.printf("Renaming file %s to %s\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("File renamed");
+    } else {
+        Serial.println("Rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\n", path);
+    if(fs.remove(path)){
+        Serial.println("File deleted");
+    } else {
+        Serial.println("Delete failed");
+    }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+    File file = fs.open(path);
+    static uint8_t buf[512];
+    size_t len = 0;
+    uint32_t start = millis();
+    uint32_t end = start;
+    if(file){
+        len = file.size();
+        size_t flen = len;
+        start = millis();
+        while(len){
+            size_t toRead = len;
+            if(toRead > 512){
+                toRead = 512;
+            }
+            file.read(buf, toRead);
+            len -= toRead;
+        }
+        end = millis() - start;
+        Serial.printf("%u bytes read for %u ms\n", flen, end);
+        file.close();
+    } else {
+        Serial.println("Failed to open file for reading");
+    }
+
+
+    file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+
+    size_t i;
+    start = millis();
+    for(i=0; i<2048; i++){
+        file.write(buf, 512);
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+    file.close();
+}
+
+void setup(){
+    Serial.begin(115200);
+    if(!SD.begin()){
+        Serial.println("Card Mount Failed");
+        return;
+    }
+    uint8_t cardType = SD.cardType();
+
+    if(cardType == CARD_NONE){
+        Serial.println("No SD card attached");
+        return;
+    }
+
+    Serial.print("SD Card Type: ");
+    if(cardType == CARD_MMC){
+        Serial.println("MMC");
+    } else if(cardType == CARD_SD){
+        Serial.println("SDSC");
+    } else if(cardType == CARD_SDHC){
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+
+    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+    listDir(SD, "/", 0);
+    createDir(SD, "/mydir");
+    listDir(SD, "/", 0);
+    removeDir(SD, "/mydir");
+    listDir(SD, "/", 2);
+    writeFile(SD, "/hello.txt", "Hello ");
+    appendFile(SD, "/hello.txt", "World!\n");
+    readFile(SD, "/hello.txt");
+    deleteFile(SD, "/foo.txt");
+    renameFile(SD, "/hello.txt", "/foo.txt");
+    readFile(SD, "/foo.txt");
+    testFileIO(SD, "/test.txt");
+    Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+    Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+}
+
+void loop(){
+
+}
+
+// #include "telemetry.h"
+// #include "apogeeprediction.h"
+// #include "flightstatus.h"
+// #include "sdlogger.h"
+// #include "ahrs.h"
+
+// SDLogger sdLogger;
+// Telemetry telemetry;
+// FlightStatus flightStatus;
+// AHRS ahrs;
+
+// double baseAlt;
+
+// void setup() {
+//   sleep(5);
+//   // put your setup code here, to run once:
+//   Serial.begin(115200);
+//   while (!Serial) {}
+//   Serial.println("Starting up");
+
+//   sdLogger.setup();
+
+//   telemetry.setupSensors();
+//   sdLogger.writeLog("Setup complete");
+//   Serial.println(telemetry.getSensorConfig().c_str());
+//   sdLogger.writeLog(telemetry.getSensorConfig());
+
+//   ahrs.begin(115200);//sample frequency 
+//   ahrs.setRotationVector(0,0,0);
+  
+//   Serial.println("Finished setup");
+// }
+
+// void loop() {
+//   TelemetryData telemData = telemetry.getTelemetry();
+  
+//   ahrs.update(telemData.sensorData["gyro"].gyro.x, 
+//               telemData.sensorData["gyro"].gyro.y, 
+//               telemData.sensorData["gyro"].gyro.z, 
+//               telemData.sensorData["acceleration"].acceleration.x, 
+//               telemData.sensorData["acceleration"].acceleration.y, 
+//               telemData.sensorData["acceleration"].acceleration.z, 
+//               telemData.sensorData["magnetometer"].magnetic.x, 
+//               telemData.sensorData["magnetometer"].magnetic.y, 
+//               telemData.sensorData["magnetometer"].magnetic.z);
+
+//   float rx, ry, rz;
+//   ahrs.getRotationVector(&rx,&ry,&rz);
+//   // Serial.printf("rx=%f \try=%f \trz=%f", rx, ry, rz);
+//   float gx, gy, gz;
+//   ahrs.getGravityVector(&gx, &gy, &gz);
+//   // Serial.printf("\tgx=%f \tgy=%f \tgz=%f", gx, gy, gz);
+
+//   AHRSMap ahrsData;
+//   ahrsData["rx"] = rx;
+//   ahrsData["ry"] = ry;
+//   ahrsData["rz"] = rz;
+//   ahrsData["gx"] = gx;
+//   ahrsData["gy"] = gy;
+//   ahrsData["gz"] = gz;
+
+//   flightStatus.newTelemetry(telemData.sensorData["acceleration"].acceleration.z, telemData.sensorData["pressure"].pressure);
+
+//   sdLogger.writeData(telemData, ahrsData, flightStatus.getStage());
+// }
