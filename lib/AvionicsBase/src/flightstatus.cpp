ffc4d772000d5f082be057b29e18001ddac1bb98
ccadena23 <803cici308@gmail.com>
2024-01-29 19:35:41 -0500

    Improved checkApogee() with frequency calculation

--- lib/AvionicsBase/src/flightstatus.cpp
+++ lib/AvionicsBase/src/flightstatus.cpp
@@ -15,20 +15,37 @@ double FlightStatus::median(std::vector<double> vec){
 }
 
 bool FlightStatus::checkLaunch() {
-    return false;
-}
-
-bool FlightStatus::checkCoast() {
-    return false;
-}
-
-bool FlightStatus::checkApogee() {
     std::vector<double> lm(altitudeDeque.cend() - 16, altitudeDeque.cend());
     std::vector<double> fm(altitudeDeque.cend() - 48, altitudeDeque.cend() - 16);
 
     double lmMed = median(lm);
     double fmMed = median(fm);
 
+    return lmMed > fmMed;
+}
+
+bool FlightStatus::checkCoast() {
+    std::vector<double> lm(altitudeDeque.cend() - 16, altitudeDeque.cend());
+    std::vector<double> fm(altitudeDeque.cend() - 48, altitudeDeque.cend() - 16);
+
+    double lmMed = median(lm);
+    double fmMED = median(fm);
+    
+    return false;
+}
+
+bool FlightStatus::checkApogee() {
+    // 2 seconds is the duration of apogeee
+    int n = hz * 2;
+    // looks at end of altitutde values
+    std::vector<double> lm(altitudeDeque.cend() - n, altitudeDeque.cend()); // last 2 s
+    std::vector<double> fm(altitudeDeque.cend() - 3* n, altitudeDeque.cend() - n); // last 2-6 s
+
+    // takes average of altitude values
+    double lmMed = median(lm);
+    double fmMed = median(fm);
+
+    // if altitude starts to drop (aka hit apogee), then the newest data will have a lower avg
     return lmMed < fmMed;
 }
 
