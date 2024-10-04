be944d3f22bf77a6f5893a28418f8868a93a4388
Sam Pupke <sampupke@gmail.com>
2024-10-04 10:35:58 -0400

    removing comments from main for real flight code

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,9 @@ void communicateVerification(); // Function prototype
 
 SDLogger sdLogger;
 Telemetry telemetry;
-// FlightStatus flightStatus(16);
-// AHRS ahrs;
-// KF2D KF;
+FlightStatus flightStatus(16);
+AHRS ahrs;
+KF2D KF;
 ServoInterface ms24;
 
 float rocketMass = 17.23;
@@ -26,9 +26,9 @@ float targetApogee = 1000; // This does nothing for now, you can ignore it
 
 double baseAlt;
 KF2D::MeasurementVector measurement;
-// TelemetryData telemData;
+TelemetryData telemData;
 
-// unsigned long previousTime;
+unsigned long previousTime;
 
 void setup()
 {
@@ -47,29 +47,29 @@ void setup()
   communicateVerification();
   
   
-  // sdLogger.writeLog("Setup complete");
-  // Serial.println(telemetry.getSensorConfig().c_str());
-  //  sdLogger.writeLog(telemetry.getSensorConfig());
+  sdLogger.writeLog("Setup complete");
+  Serial.println(telemetry.getSensorConfig().c_str());
+   sdLogger.writeLog(telemetry.getSensorConfig());
 
-  // ahrs.begin(); // param: sampling frequency
+  ahrs.begin(); // param: sampling frequency
   // the next 2 lines should be run when launch state is detected instead of at start
 
-  // measurement = {telemData.sensorData["altitude"].altitude, (-1)*telemData.sensorData["acceleration"].acceleration.z}; //want y then ay
-  // KF.InitializeKalmanFilter(measurement);
+  measurement = {telemData.sensorData["altitude"].altitude, (-1)*telemData.sensorData["acceleration"].acceleration.z}; //want y then ay
+  KF.InitializeKalmanFilter(measurement);
 
   pinMode(LED_BUILTIN, OUTPUT);
 
 
   Serial.println("Finished setup");
   digitalWrite(LED_BUILTIN, HIGH);
-  /// previousTime = millis();
+  previousTime = millis();
 }
 
 void loop()
 {
 
-  // telemData = telemetry.getTelemetry();
-  // Serial.println("Got telemetry data");
+  telemData = telemetry.getTelemetry();
+  //Serial.println("Got telemetry data");
   /*
   Serial.println("Telemetry data:");
   for (auto const& sensor : telemData.sensorData)
@@ -87,79 +87,79 @@ void loop()
   */
 
   // Fix orientation of LIS3MDL to LSM6DSOX
-  // telemData.sensorData["magnetometer"].magnetic.x = telemData.sensorData["magnetometer"].magnetic.y * -1;
-  // telemData.sensorData["magnetometer"].magnetic.y = telemData.sensorData["magnetometer"].magnetic.x;
+  telemData.sensorData["magnetometer"].magnetic.x = telemData.sensorData["magnetometer"].magnetic.y * -1;
+  telemData.sensorData["magnetometer"].magnetic.y = telemData.sensorData["magnetometer"].magnetic.x;
 
-  // ahrs.update(telemData.sensorData["gyro"].gyro.x,
-  //             telemData.sensorData["gyro"].gyro.y,
-  //             telemData.sensorData["gyro"].gyro.z,
-  //             telemData.sensorData["acceleration"].acceleration.x,
-  //             telemData.sensorData["acceleration"].acceleration.y,
-  //             telemData.sensorData["acceleration"].acceleration.z,
-  //             telemData.sensorData["magnetometer"].magnetic.x,
-  //             telemData.sensorData["magnetometer"].magnetic.y,
-  //             telemData.sensorData["magnetometer"].magnetic.z);
+  ahrs.update(telemData.sensorData["gyro"].gyro.x,
+              telemData.sensorData["gyro"].gyro.y,
+              telemData.sensorData["gyro"].gyro.z,
+              telemData.sensorData["acceleration"].acceleration.x,
+              telemData.sensorData["acceleration"].acceleration.y,
+              telemData.sensorData["acceleration"].acceleration.z,
+              telemData.sensorData["magnetometer"].magnetic.x,
+              telemData.sensorData["magnetometer"].magnetic.y,
+              telemData.sensorData["magnetometer"].magnetic.z);
 
-  // float rx, ry, rz;
-  //  ahrs.getRotationVector(&rx, &ry, &rz);
-  //  Serial.printf("rx=%f \try=%f \trz=%f", rx, ry, rz); //still very rough, try to avoid using if at all possible
-  // float gx, gy, gz;
-  //  ahrs.getGravityVector(&gx, &gy, &gz);
-  //  Serial.printf("\tgx=%f \tgy=%f \tgz=%f", gx, gy, gz);
+  float rx, ry, rz;
+   ahrs.getRotationVector(&rx, &ry, &rz);
+   Serial.printf("rx=%f \try=%f \trz=%f", rx, ry, rz); //still very rough, try to avoid using if at all possible
+  float gx, gy, gz;
+   ahrs.getGravityVector(&gx, &gy, &gz);
+   Serial.printf("\tgx=%f \tgy=%f \tgz=%f", gx, gy, gz);
 
-  // AHRSMap ahrsData;
-  // ahrsData["rx"] = rx;
-  // ahrsData["ry"] = ry;
-  // ahrsData["rz"] = rz;
-  // ahrsData["gx"] = gx;
-  // ahrsData["gy"] = gy;
-  // ahrsData["gz"] = gz;
+  AHRSMap ahrsData;
+  ahrsData["rx"] = rx;
+  ahrsData["ry"] = ry;
+  ahrsData["rz"] = rz;
+  ahrsData["gx"] = gx;
+  ahrsData["gy"] = gy;
+  ahrsData["gz"] = gz;
 
-  // Calculate the orientation using the gravity vector
-  // double euler[3];
-  // euler[0] = atan2(gy, gz);
-  // euler[1] = atan2(-gx, sqrt(gy * gy + gz * gz));
-  // euler[2] = 0;
+  //Calculate the orientation using the gravity vector
+  double euler[3];
+  euler[0] = atan2(gy, gz);
+  euler[1] = atan2(-gx, sqrt(gy * gy + gz * gz));
+  euler[2] = 0;
 
   // Vert Velocity Prediction
-  // double accel[] = {telemData.sensorData["acceleration"].acceleration.x,
-  //                  telemData.sensorData["acceleration"].acceleration.y,
-  //                  telemData.sensorData["acceleration"].acceleration.z};
-  // double euler[] = {ahrsData["rx"], ahrsData["ry"], ahrsData["rz"]};
-  ////double vAccel = ApogeePrediction::getVertAccel(accel, euler);
+  double accel[] = {telemData.sensorData["acceleration"].acceleration.x,
+                    telemData.sensorData["acceleration"].acceleration.y,
+                    telemData.sensorData["acceleration"].acceleration.z};
+  //double euler[] = {ahrsData["rx"], ahrsData["ry"], ahrsData["rz"]};
+  double vAccel = ApogeePrediction::getVertAccel(accel, euler);
   // double vAccel = telemData.sensorData["acceleration"].acceleration.z-9.81; // This "vAccel" value is only when the board is facing upwards, not at any other orientation
 
   // The next 3 lines should be run on loop after launch is detected
-  //// measurement = {telemData.sensorData["altitude"].altitude, (float)(-9.81+telemData.sensorData["acceleration"].acceleration.z)}; //want y then ay- NOT g //eventually needs to move to AHRS vertical accel
-  // measurement = {telemData.sensorData["altitude"].altitude, (float)(vAccel)};
-  // Serial.printf("altitude = %f, \t y-acceleration = %f\t", measurement[0], measurement[1]);
-  // unsigned long nowTime = millis();
-  // float dt = previousTime - nowTime;
-  // previousTime = nowTime;
-  // float dt = 0.25;
-  // KF.Update(measurement);
-  // KF.Predict();
-  // Serial.printf("x_hat: \t%f m, \t%f m/s, \t%f m/s/s", KF.x_hat[0], KF.x_hat[1], KF.x_hat[2]);
+  // // measurement = {telemData.sensorData["altitude"].altitude, (float)(-9.81+telemData.sensorData["acceleration"].acceleration.z)}; //want y then ay- NOT g //eventually needs to move to AHRS vertical accel
+  measurement = {telemData.sensorData["altitude"].altitude, (float)(vAccel)};
+  Serial.printf("altitude = %f, \t y-acceleration = %f\t", measurement[0], measurement[1]);
+  unsigned long nowTime = millis();
+  float dt = previousTime - nowTime;
+  previousTime = nowTime;
+  // // float dt = 0.25;
+  KF.Update(measurement);
+  KF.Predict();
+  Serial.printf("x_hat: \t%f m, \t%f m/s, \t%f m/s/s", KF.x_hat[0], KF.x_hat[1], KF.x_hat[2]);
 
-  // KFData kfData = {
-  //     acceleration: KF.x_hat[2],
-  //     velocity: KF.x_hat[1],
-  //     drift: KF.x_hat[0]
-  // };
+  KFData kfData = {
+      acceleration: KF.x_hat[2],
+      velocity: KF.x_hat[1],
+      drift: KF.x_hat[0]
+  };
 
   // testflight app and link on slack
 
   // Apogee Prediction
   // REPLACE APPRED W SENSOR AY
   // double predApogee = apogeePrediction.predictApogee(KF.x_hat[1], telemData.sensorData["pressure"].pressure, telemData.sensorData["temperature"].temperature, telemData.sensorData["altitude"].altitude);
-  // double predApogee = ApogeePrediction::newPredictApogee(KF.x_hat[1], telemData.sensorData["altitude"].altitude, telemData.sensorData["pressure"].pressure, telemData.sensorData["temperature"].temperature, dragCoefficent, rocketMass, crossArea);
-  // Serial.println("Apogee Prediction: " + String(predApogee) + "m");
-  // Serial.println("Getting flight status");
-  // flightStatus.newTelemetry(telemData.sensorData["acceleration"].acceleration.z, telemData.sensorData["altitude"].altitude);
-  // Serial.printf("Flight Status: %s\n", flightStatus.getStageString().c_str());
+  double predApogee = ApogeePrediction::newPredictApogee(KF.x_hat[1], telemData.sensorData["altitude"].altitude, telemData.sensorData["pressure"].pressure, telemData.sensorData["temperature"].temperature, dragCoefficent, rocketMass, crossArea);
+  Serial.println("Apogee Prediction: " + String(predApogee) + "m");
+  Serial.println("Getting flight status");
+  flightStatus.newTelemetry(telemData.sensorData["acceleration"].acceleration.z, telemData.sensorData["altitude"].altitude);
+  Serial.printf("Flight Status: %s\n", flightStatus.getStageString().c_str());
 
   // Serial.println("VAccel: " + String(vAccel));
-  // sdLogger.writeData(telemData, kfData, vAccel, predApogee, flightStatus.getStageString());
+  sdLogger.writeData(telemData, kfData, vAccel, predApogee, flightStatus.getStageString());
 
   
 }
