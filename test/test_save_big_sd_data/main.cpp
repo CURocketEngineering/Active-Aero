da60ac5a2767e41551499ae15349585779a6a571
Elan456 <telan4892@gmail.com>
2025-04-05 14:06:32 -0400

    Fix platformio.ini lib dependencies, add sd card saving test

---
+++ test/test_save_big_sd_data/main.cpp
@@ -0,0 +1,33 @@
+#include <unity.h>
+#include <SPI.h>
+#include <SdFat.h>
+#include <Adafruit_SPIFlash.h>
+#include "data_handling/DataSaverBigSD.h"
+#include "data_handling/DataPoint.h"
+#include "data_handling/DataNames.h"
+
+DataSaverBigSD dataSaver;
+
+void test_save_data_point() {
+    TEST_ASSERT_TRUE(dataSaver.begin());
+
+    DataPoint dp (0, 10); 
+    int result = dataSaver.saveDataPoint(dp, 0x01);
+    TEST_ASSERT_EQUAL(0, result); // Check if saveDataPoint returns 0 (success)
+}
+
+void setup() {
+    Serial.begin(115200);
+    while (!Serial) {
+        delay(100); // wait for native USB
+    }
+
+    UNITY_BEGIN();
+    Serial.println("Starting test...");
+    TEST_ASSERT_TRUE(dataSaver.begin());
+    UNITY_END();
+}
+
+void loop() {
+    // nothing to do here
+}
