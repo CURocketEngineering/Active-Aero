23dae54d78392bb330796943b343a3a42b98f38a
Mikey Schoonmaker <mcschoo@clemson.edu>
2025-04-17 16:34:18 -0400

    rip communicate verification, -> commsVerification

--- include/core/CommunicationVerification.h
+++ include/core/CommunicationVerification.h
@@ -1,3 +1,8 @@
 #pragma once
 
-void communicateVerification(bool sd_init);
+/***
+ * initally deploys fins to show that we are in the communicate verification function
+ * retracts fins before entering loop
+ * if fins deploy after that point, we have an error
+ */
+void commsVerification(bool sd_init);
--- src/CommunicationVerification.cpp
+++ src/CommunicationVerification.cpp
@@ -1,3 +1,23 @@
-#include "config.h"
+#include "common/config.h"
 #include "common/globals.h"
-#include "simulation/Serial_Sim.h"
+
+
+/***
+ * initally deploys fins to show that we are in the communicate verification function
+ * retracts fins before entering loop
+ * if fins deploy after that point, we have an error
+ */
+void commsVerification(bool sd_init) {
+    ms24.setAngle(MAX_DEPLOYMENT_ANGLE);
+    delay(COMMUNICATION_VERIFICATION_DELAY);
+    ms24.setAngle(MIN_DEPLOYMENT_ANGLE);
+    delay(COMMUNICATION_VERIFICATION_DELAY);
+
+    SensorsActivated sensorsActivated = telemetry.getSensorsActivated();
+    std::vector<bool> verifiables = {sensorsActivated.mag, sensorsActivated.bmp, sensorsActivated.imu, sd_init};
+    for (bool verifiable : verifiables) {
+        ms24.setAngle(verifiable ? MIN_DEPLOYMENT_ANGLE : MAX_DEPLOYMENT_ANGLE);
+        delay(COMMUNICATION_VERIFICATION_DELAY);
+    }
+    delay(COMMUNICATION_VERIFICATION_DELAY);
+}
